a57656f3968ace266f5eb9c86018a0eb378d1b76
katKoufaki <72813458+katKoufaki@users.noreply.github.com>
2021-01-17 19:13:35 +0200

    Add files via upload

--- AirportStuff.java
+++ AirportStuff.java
@@ -1,65 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.time.*;
-
-public class AirportStuff extends Person {
-
-    // A map that has as key one of the days of the week, and as value an arraylist which has inside an array with 2 rows
-    // The first row is about the time when employee starts working this day and the second is about the time of the end of working
-    private HashMap<DayOfWeek, ArrayList<LocalTime[]>> workHoursMap; 
-
-    /**
-     * Constructor of the class
-     * Creates an employee (storeStuff, gateStuff or checkInStuff) with the specified elements which are inherited by class Person
-     * @see Person
-     * @param ssn The SSN of the employee
-     * @param name The first name of the employee
-     * @param lastName The last name of the employee
-     * @param address The address of the employee
-     * @param phone The phone of the employee
-     */
-    public AirportStuff(String ssn, String name, String lastName, String address, String phone) {
-        super(ssn, name, lastName, address, phone);
-        this.workHoursMap = new  HashMap<DayOfWeek,ArrayList<LocalTime[]>>();
-        setWorkHoursMap();
-    }
-
-
-    /**
-     * Sets the work hours of the employee for everyday
-     */
-    public void setWorkHoursMap() {
-        for(DayOfWeek day : DayOfWeek.values()){
-            workHoursMap.put(day, new ArrayList<LocalTime[]>());
+public class AirportStuff {
+    public boolean isWorking(LocalDateTime dateTime){
+        DayOfWeek day = dateTime.getDayOfWeek();
+        LocalTime time = dateTime.toLocalTime();
+        for(LocalTime[] localtimes : this.workHoursMap.get(day)){
+            if(time.isAfter(localtimes[0]) && time.isBefore(localtimes[1])){
+                return true;
+            }
         }
+        return false;
     }
-
-
-    /**
-     * Adds the work hour of one day of the week for an employee
-     * @param day DayofWeek containing a day of the week
-     * @param startTime LocalTime containing the time when employee starts working
-     * @param endTime LocalTime containing the time when employee ends working
-     * @return HashMap representing the weekly work hours of an employee
-     */
-    public boolean addWorkHours(DayOfWeek day, LocalTime startTime, LocalTime endTime) {
-        return this.workHoursMap.get(day).add(new LocalTime[]{startTime, endTime});
-    }
-
-
-    /**
-     * Adds the work hour of one day of the week for an employee
-     * @param day DayofWeek containing a day of the week
-     * @param startTime String containing the time when employee starts working
-     * @param endTime String containing the time when employee ends working
-     * @return HashMap representing the weekly work hours of an employee
-     */
-    public boolean addWorkHours(DayOfWeek day, String startTime, String endTime) {
-        // Convert String startTime to LocalTime
-        LocalTime startTimeDate = LocalTime.parse(startTime);
-        // Convert String endTime to LocalTime
-        LocalTime endTimeDate = LocalTime.parse(endTime);
-        return this.workHoursMap.get(day).add(new LocalTime[]{startTimeDate, endTimeDate});
-    }
-
 }
--- Flight.java
+++ Flight.java
@@ -1,197 +1,49 @@
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+public class Flight {
+    public ArrayList<Person> findCloseContactsOfPassenger(String ssn){
+        ArrayList<Person> closeContacts = new ArrayList<Person>();
+        try {
+            LocalDateTime time = this.getTicketBySSN(ssn).getCheckInDateTime();
 
-public class Flight implements Serializable {
+            closeContacts.add(this.getDepartureAirport().getWorkingEmployee(time));
 
-    // A unique id for each flight
-    private static int flightId = 0;
-    private Airport departureAirport;
-    private Airport destinationAirport;
-    private LocalDateTime departureDateTime;
-    private LocalDateTime destinationDateTime;
-    // An ArrayList that contains objects Person which are the crew of this flight
-    private ArrayList<Person> flightCrew;
-    // An ArrayList that contains objects Ticket which are the tickets of this flight
-    private ArrayList<Ticket> tickets;
-
-
-    /**
-     * Constructor of the class
-     * Creates a flight with the specified gate's name, departure and destination airport's ICAO and flight's date
-     * @param departureAirport The airport where flight departs
-     * @param destinationAirport The airport where flight arrives
-     * @param departureDateTime The date and time of take off
-     * @param destinationDateTime The date and time of landing
-     */
-    public Flight(Airport departureAirport, Airport destinationAirport, String departureDateTime, String destinationDateTime) {
-            this.departureAirport = departureAirport;
-            this.destinationAirport = destinationAirport;
-            setDepartureDateTime(departureDateTime);
-            setDestinationDateTime(destinationDateTime);    
-            this.flightCrew = new ArrayList<Person>();
-            this.tickets = new ArrayList<Ticket>();
-            // when create a flight object, flight id increase by 1
-            ++flightId;
-    }
-
-
-    /**
-     * Gets the id of the flight
-     * @return int representing the flight id
-     */
-    public static int getFlightId() {
-		return flightId;
-	}
-
-
-    /**
-     * Sets the aiport where flight departs
-     * @param departureAirport Object Airport containing the airport of departure
-     * @see Airport
-     */
-    public void setDepartureAirport(Airport departureAirport) {
-        this.departureAirport = departureAirport;
-    }
-
-
-    /**
-     * Gets the aiport where flight departs
-     * @return Airport representing the airport of departure
-     */
-    public Airport getDepartureAirport() {
-        return this.departureAirport;
-    }
-
-
-    /**
-     * Sets the aiport where flight arrives
-     * @param destinationAirport Object Airport containing the airport of destination
-     * @see Airport
-     */
-    public void setDestinationAirport(Airport destinationAirport) {
-		this.destinationAirport = destinationAirport;
-	}
-
-
-    /**
-     * Gets the aiport where flight arrives
-     * @return Airport representing the airport of destination
-     */
-    public Airport getDestinationAirport() {
-		return this.destinationAirport;
-	}
-
-
-    /**
-     * Set the date and time when airplane takes off
-     * @param departureDateTime String containing the date and time of this flight's departure
-     */
-    public void setDepartureDateTime(String departureDateTime) {
-        // Convert String departureDateTime to LocalDateTime
-        this.departureDateTime = LocalDateTime.parse(departureDateTime);
-    }
-  
-
-    /**
-     * Gets the date and time of this flight's departure
-     * @return LocalDateTime representing the date and time of this flight's departure
-     */
-    public LocalDateTime getDepartureDateTime() {
-		return departureDateTime;
-	}
-
-
-    /**
-     * Set the date and time when airplane lands
-     * @param destinationDateTime String containing the date and time of this flight's destination
-     */
-    public void setDestinationDateTime(String destinationDateTime) {
-        // Convert String destinationDateTime to LocalDateTime
-        this.destinationDateTime = LocalDateTime.parse(destinationDateTime);
-    }
-
-
-    /**
-     * Gets the date and time of this flight's destination
-     * @return LocalDateTime representing the date and time of this flight's destination
-     */
-    public LocalDateTime getDestinationDateTime() {
-		return destinationDateTime;
-	}
-
-
-    /**
-     * Adds an object Person to the ArrayList flightCrew
-     * Check if this object already exists into the ArrayList flightCrew
-     * @param employee Object Person containing an employee of this flight
-     * @see Person
-     */
-    public void addFlightCrew(Person employee) {
-        if (!this.flightCrew.contains(employee)) {
-            this.flightCrew.add(employee);
-        } else {
-            System.out.println("This employee already exists in this flight");
+            for(Person employee : this.getFlightCrew()){
+                closeContacts.add(employee);
+            }
+            for(Person employee : this.getPasengers()){
+                closeContacts.add(employee);
+            }
+        } catch(NullPointerException e) {
+            System.out.println("No ticket found for this ssn");
+            return null;
         }
+        return closeContacts;
     }
 
+    public ArrayList<Person> findCasualContactsOfPassenger(String ssn){
+        ArrayList<Person> casualContacts = new ArrayList<Person>();
+        LocalDateTime flightDateTime = this.getDepartureDateTime();
 
-    /**
-     * Gets the ArrayList flightCrew with this flight's crew
-     * @return ArrayList<Person> containing objects Person representing the crew of this flight
-     * @see Person
-     */
-    public ArrayList<Person> getFlightCrew() {
-        return this.flightCrew;
-    }
-
-
-    /**
-     * Adds an object Ticket to the ArrayList tickets
-     * Check if this object already exists into the ArrayList tickets
-     * @param ticket Object Ticket containing a ticket of this flight
-     * @see Ticket
-     */
-    public void addTicket(Ticket ticket) {
-        if(!this.tickets.contains(ticket)) {
-            this.tickets.add(ticket);
-        } else {
-            System.out.println("This ticket is already exists");
-        }
-    }
-
-
-    /**
-     * Gets the ArrayList tickets with all tickets of this flight
-     * @return ArrayList<Ticket> containing objects Ticket representing the tickets of this flight
-     * @Ticket
-     */
-    public ArrayList<Ticket> getTickets() {
-        return this.tickets;
-    }
-
-
-    /**
-     * Gets the travel of the flight
-     * @return String representing the departure and destination of the flight
-     */
-    public String getTravel() {
-        return getDepartureAirport().getAirportICAO() + "-" + getDestinationAirport().getAirportICAO();
-    }
-
-    /**
-     * Gets the ticket of a specific passenger
-     * @param ssn String containing the ssn of passenger that we search for his ticket
-     * @return Ticket representing the ticket of passenger with this ssn
-     * @see Ticket
-     */
-    public Ticket getTicketBySSN(String ssn){
-        for (Ticket ticket : getTickets()){
-            if(ticket.getPassenger().getSSN().equals(ssn)){
-                return ticket;
+        for(AirportStuff employee : this.getTicketBySSN(ssn).getDepartureGate().getSectionStuff()){
+            if(employee.isWorking(flightDateTime)){
+                casualContacts.add(employee);
             }
         }
-        return null;
+
+            for(VisitedStore visitedStore : this.getTicketBySSN(ssn).getDepartureVisitedStores()){
+                for(AirportStuff employee : visitedStore.getStore().getSectionStuff()){
+                    if(employee.isWorking(visitedStore.getEntranceDateTime())){
+                        casualContacts.add(employee);
+                    }
+                }
+            }
+        return casualContacts;
     }
 
+    public ArrayList<Person> getPasengers(){
+        ArrayList<Person> passengers = new ArrayList<Person>();
+        for(Ticket ticket : this.getTickets()){
+            passengers.add(ticket.getPassenger());
+        }
+        return passengers;
+    }
 }
